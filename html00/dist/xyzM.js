75e4a83441d21abc9bdbab47fcda2341a58ce291
Daniel Salinas <salinas.programador@gmail.com>
2025-06-09 00:06:21 -0600

    Se agrego submodulo Ploceus26

---
+++ html00/dist/xyzM.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y: "+o),"fin y"}function e(o){return n("x"),console.log("x: "+o),"fin x"}function r(o){return"fin xx"}function l(o){return console.log("z: "+o),"fin z"}document.getElementById("app");console.log(e("abc"));console.log(r("def"));console.log(n("ghi"));console.log(l("jkl"));})();
+//# sourceMappingURL=xyzM.js.map
--- html00/formulario.js
+++ html00/formulario.js
@@ -1,15 +1,20 @@
 console.log("Inicio");
 
+const mostrar = () => {
+    console.log("los parametros");
+    let user = document.getElementById("user");
+    let password = document.getElementById("password");
+    console.log(user.value);
+    console.log(password.value);
+
+    let formulario = document.getElementsByTagName("form");
+    console.log(formulario)
+    user.setAttribute("name","q")
+    password.setAttribute("name","password")
+    formulario[0].setAttribute("action","https://google.com/search")
+    formulario[0].submit();
+};
+
 const boton = document.getElementById("Enviar")
+boton.addEventListener("click", mostrar);
 
-console.log(boton);
-
-boton.addEventListener("click", 
-    () => {
-        console.log("los parametros")
-        let user = document.getElementById("user")
-        let password = document.getElementById("password")
-        console.log(user.value)
-        console.log(password.value)
-    }
-)
